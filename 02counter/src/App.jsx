4f81c6fb25a2bdb344de8157d8da84ff85173783
Ritik-Sharma2311 <155327536+Ritik-Sharma2311@users.noreply.github.com>
2024-02-08 20:24:08 +0000

    Hooks and project

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState, } from 'react'    //React is responsible for all the changes in the UI
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  let [counter, setCounter] =  useState(15)
+  // Here, counter=> variable &
+  //       setCounter/abcCounter=> function
+  // setCounter is the funtion responsible to update the counter variable
+
+  // let counter= 15
+
+  const addValue = () => {
+    // counter = counter + 1
+    setCounter(counter + 1)
+
+  }
+
+  const removeValue = ()=> {
+
+    setCounter(counter-1)
+  }
+
+
+  return (
+    <>
+      <h1>Chai aur React</h1>
+      <h2>CounterValue: {counter}</h2>
+
+      <button
+      onClick={addValue}
+      >Add value {counter}</button>
+      <br/>
+      <button
+      onClick={removeValue}
+      >Remove value {counter}</button>
+      <p>footer: {counter} </p>
+
+    </>       
+  )
+}
+
+export default App
