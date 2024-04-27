30dbef9831620074f500d5e2739f120acfb16e32
Ritik-Sharma2311 <155327536+Ritik-Sharma2311@users.noreply.github.com>
2024-04-27 22:35:20 +0000

    Dynamic data: React Router

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -2,31 +2,53 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {RouterProvider, createBrowserRouter} from  'react-router-dom'
+import {Route, RouterProvider, createBrowserRouter, createRoutesFromElements} from  'react-router-dom'
 import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
+import Contact from './components/Contact/Contact.jsx'
+import User from './components/User/User.jsx'
+
+//We can prevent writting the address of each COMPONENT again n again by simply writting/exporting these from an [index.js] file within Components.
+ 
 
 
-// Now we are making a router , which we will pass later downwards
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout/>,
-    children: [
-      {
-        path: "",
-        element: <Home/>
-      },
-      {
-        path:"about",
-        element:<About/>
-      }
-      
-    ]
+// Below are 2ways of making a router,which we will pass later downwards
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Layout/>,
+//     children: [
+//       {
+//         path: "",
+//         element: <Home/>
+//       },
+//       {
+//         path:"about",
+//         element:<About/>
+//       }
+//       ,
+//       {
+//         path:"contact",
+//         element:<Contact/>
+//       }
+//     ]
 
-  }
-])
+//   }
+// ])
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout/>}>
+     <Route path='' element={<Home/>}/>
+     <Route path='about' element={<About/>}/>
+     <Route path='contact' element={<Contact/>}/>
+     <Route path='user/:userid' element={<User/>}/>
+
+    </Route>
+
+  )
+)
 
 
 //Here, we are passing the above made router!
