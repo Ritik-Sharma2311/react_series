779e954b08a60a61954951c03d4934a93982f315
Ritik-Sharma2311 <155327536+Ritik-Sharma2311@users.noreply.github.com>
2024-05-02 11:15:41 +0000

    Conntext Api 08miniContext

---
+++ 08miniContext/src/context/UserContext.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const UserContext = React.createContext()
+//context is a kind of Global Varible
+
+export default UserContext;
+
+//once Context is made ,we'll have to make a provider (with <></> syntax)
+
