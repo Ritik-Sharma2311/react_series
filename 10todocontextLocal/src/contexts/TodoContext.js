e23ec913f544a0195e7c87d2c8686403dcfe7055
Ritik-Sharma2311 <155327536+Ritik-Sharma2311@users.noreply.github.com>
2024-05-20 18:10:16 +0000

    TodoContext_changes

--- 10todocontextLocal/src/contexts/TodoContext.js
+++ 10todocontextLocal/src/contexts/TodoContext.js
@@ -1,12 +1,26 @@
+import { data } from "autoprefixer"
 import React, {createContext, useContext} from "react"
 
 export const TodoContext = createContext({
-  
-})
+  todos: [
+    {
+        id: 1,
+        todo: "Todo msg",
+        completed: false,
+    }
+  ],
+  addTodo: (todo) => {},
+  updatedTodo: (id, todo) =>{},
+  deleteTodo : (id) => {},
+  toggleComplete : (id) => {}
+}
+// here in the Context we have stated/talked about what values/propertires are present and what methods are here
+)
+
 
 export const useTodo = () => {
     return useContext(TodoContext)
 }
 
-export const Todoprovider = TodoContext.Provider
+export const TodoProvider = TodoContext.Provider
 
