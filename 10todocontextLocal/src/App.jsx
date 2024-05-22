788f083e6628cbf78e88925a452dd59332e74fbb
Ritik-Sharma2311 <155327536+Ritik-Sharma2311@users.noreply.github.com>
2024-05-22 14:00:58 +0000

    Functionality_done

--- 10todocontextLocal/src/App.jsx
+++ 10todocontextLocal/src/App.jsx
@@ -6,16 +6,32 @@ function App() {
 const [todos, setTodos] = useState([])
 
 const addTodo = (todo) => {
-  setTodos((prev) => [{id: Date.now(),...todo}, ...prev] )
+  setTodos((prev) => [{id : Date.now(), ...todo}, ...prev] )
 }
 
+
 const updateTodo = (id, todo) => {
-  setTodos((prev) => )
+ setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo) ))
+
+// setTodos((prev) => prev.map((eachVal) => {
+//   if(eachVal.id === id){
+//     todo
+//   }
+// }))
+ }
+
+
+const deleteTodo = (id) => {
+ setTodos((prev) => prev.filter((prevTodo) => prevTodo.id !== id))
+}
+
+const toggleComplete = (id) => {
+  setTodos((prev)=> prev.map((prevTodo) => prevTodo.id === id ? {...prevTodo, completed: !prevTodo.completed } : prevTodo ))
 }
 
 
   return (
-    <TodoProvider value={{todos, addTodo, updatedTodo, deleteTodo, toggleComplete }}>
+    <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete }}>
    <div className="bg-[#172842] min-h-screen py-8">
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
