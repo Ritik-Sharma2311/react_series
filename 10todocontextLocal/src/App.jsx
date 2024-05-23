1c263023bc8e4bf6dd04f6e9fcb9d3ae709ae2f5
Ritik-Sharma2311 <155327536+Ritik-Sharma2311@users.noreply.github.com>
2024-05-23 08:12:22 +0000

    Done with Api Context

--- 10todocontextLocal/src/App.jsx
+++ 10todocontextLocal/src/App.jsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import { TodoProvider } from './contexts'
+import TodoForm from './components/TodoForm'
+import TodoItem from './components/TodoItem'
 
 function App() {
 const [todos, setTodos] = useState([])
 
 const addTodo = (todo) => {
-  setTodos((prev) => [{id : Date.now(), ...todo}, ...prev] )
+  setTodos((prev) => [{ ...todo}, ...prev] )
 }
 
 
@@ -49,9 +51,16 @@ if(todos && todos.length > 0){
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
                     <div className="mb-4">
                         {/* Todo form goes here */} 
+                        <TodoForm/>
                     </div>
                     <div className="flex flex-wrap gap-y-3">
+                        
                         {/*Loop and Add TodoItem here */}
+                         {todos.map((todo) => (
+                          <div key={todo.id} className="w-full">
+                           <TodoItem todo={todo}/>
+                          </div>
+                         ))}
                     </div>
                 </div>
             </div>
